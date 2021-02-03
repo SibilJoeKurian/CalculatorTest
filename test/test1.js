40a4dd60e7a2453009cc3420ef049a2d43487682
sibil.kurian@gmail.com <sibil.kurian@gmail.com>
2021-02-03 10:53:36 -0500

    added script

--- test/test1.js
+++ test/test1.js
@@ -1,27 +1,27 @@
-const { page_objects_path } = require("../nightwatch.conf");
+// const { page_objects_path } = require("../nightwatch.conf");
 
-module.exports = {
-    beforeEach: function (browser) {
-        browser.url('https://output.jsbin.com/hudape/1/')
-    },
-    'Check calculator page loading': function (browser) {
-        browser
-            .assert.titleContains('JS Bin')
-            .useXpath()
-    },
+// module.exports = {
+//     beforeEach: function (browser) {
+//         browser.url('https://output.jsbin.com/hudape/1/')
+//     },
+//     'Check calculator page loading': function (browser) {
+//         browser
+//             .assert.titleContains('JS Bin')
+//             .useXpath()
+//     },
       
-    'Test 6 Result of a previous operation when the result is 0 [0*6*6=0]':function(browser){
-        browser.page.calculator()
-            .press(0).press('×')
-            .press(6).press('×').press(6)
-            .press('=')
-            .execute(function() {
-                return document.querySelector('div#output').innerText
-            }, [], function(result) {
-                console.log(result)
-                this.assert.equal(result.value, '0')
-            }).clear()
-    },
+//     'Test 6 Result of a previous operation when the result is 0 [0*6*6=0]':function(browser){
+//         browser.page.calculator()
+//             .press(0).press('×')
+//             .press(6).press('×').press(6)
+//             .press('=')
+//             .execute(function() {
+//                 return document.querySelector('div#output').innerText
+//             }, [], function(result) {
+//                 console.log(result)
+//                 this.assert.equal(result.value, '0')
+//             }).clear()
+//     },
     //'Test 18 subtraction of two ten digit number'
 
     // 'add -ve nuumber to a +ve number -11111111 +22222222': function (browser) {
@@ -41,4 +41,4 @@ module.exports = {
 
     // }
 
-}
+//}
